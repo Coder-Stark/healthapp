3458fcbf81ab2c73d29e05353f3cd49870f1b352
shivamkumar <shivamkumar251029@gmail.com>
2025-09-26 10:04:35 +0530

    initial Commit

--- App.tsx
+++ App.tsx
@@ -1,37 +1,13 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
+ * Simple React Native App
  */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text style={styles.text}>Hello World!</Text>
     </View>
   );
 }
@@ -39,6 +15,14 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
   },
 });
 
---
+++ Directory Tree.js
@@ -0,0 +1,26 @@
+/*
+healthapp
+ ┣ _tests__
+ ┃ ┗ App.test.tsx
+ ┣ .bundle
+ ┃ ┗ config
+ ┣  android
+ ┣  ios
+ ┣  node_modules
+ ┣ .eslintrc.js
+ ┣ .gitignore
+ ┣ .prettierrc.js
+ ┣ .watchmanconfig
+ ┣ app.json
+ ┣ App.tsx
+ ┣ babel.config.js
+ ┣ Gemfile
+ ┣ index.js
+ ┣ jest.config.js
+ ┣ metro.config.js
+ ┣ package-lock.json
+ ┣ package.json
+ ┣ README.md
+ ┗ tsconfig.json
+
+*/
